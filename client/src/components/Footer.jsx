a7aec2d289d8341b72e4935854a8eeb64eb54044
Ashfaq Syed <ashfaqsyed282@gmail.com>
2026-08-10 11:35:19 +0530

    Create React frontend components

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+      <p>TravelScape</p>
+      <p>Explore More. Travel Better.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>TravelScape</h2>
+
+      <div>
+        <a href="/">Home</a>
+        <a href="/">Destinations</a>
+        <a href="/">About</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
